750391ca35bf2e4e73643e19f1e642dc9649862f
river <river@river.local>
2017-02-06 21:16:21 +0800

    init project

---
+++ README.md
@@ -0,0 +1,38 @@
+##Note
+在seajs配置文件后追加配置，让seajs知道静态资源编译后的路径。
+```
+seajs.config({map:[
+    ['js/a.js', 'js/a-8cd72c35.js'],
+    ['js/b.js', 'js/b-8cd72c35.js']
+]});
+```
+追加配置后，重新对seajs配置文件签名哈希，并更新到manifest流中。
+
+##Install
+```
+npm install --save-dev gulp-seajs-config-map
+```
+
+##Usage
+```
+var gulp = require('gulp');
+var rev = require('gulp-rev'),
+var seajsRev = require('gulp-seajs-config-map');
+
+gulp.task('build', function() {
+    return gulp.src('./testfiles/*.js', {base:'./testfiles'})
+        .pipe(rev())
+        .pipe(gulp.dest(./dist))
+        .pipe(rev.manifest())
+        .pipe(seajsRev({configFile:'dist/js/sea-config.js'}))
+        .pipe(gulp.dest(./dist));
+});
+```
+
+##Options
+```
+configFile: 需要追加seajs配置的文件相对路径，相对于manifest文件的路径。
+```
+
+##Licence
+MIT
---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+var crypto = require('crypto');
+var path = require('path');
+var gutil = require('gulp-util');
+var file = require('vinyl-file');
+var through = require('through2');
+var fs = require('fs');
+
+function md5(str) {
+	return crypto.createHash('md5').update(str).digest('hex');
+}
+
+function getNewConfig(opts, cb) {
+	file.read(opts.path, opts, function (err, file) {
+		if (err) {
+			// not found
+			if (err.code === 'ENOENT') {
+				cb(null, new gutil.File(opts));
+			} else {
+				cb(err);
+			}
+
+			return;
+		}
+
+		cb(null, file);
+	});
+}
+
+var plugin = function (options) {
+	options = options || {base:'build', configFile:'js/seajs-config.js'};
+
+	return through.obj(function (file, enc, cb) {
+		if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			cb(new gutil.PluginError('gulp-seajs-rev', 'Streaming not supported'));
+			return;
+		}
+		var seajsConfigPath = path.join(options.base, options.configFile);
+
+		var manifest = JSON.parse(file.contents);
+		var jsManifest = [];
+
+		for(var fileName in manifest) {
+			//if (fileName.endsWith('.js')) {
+				jsManifest.push([fileName, manifest[fileName]]);
+			//}
+		}
+
+		var configString = 'seajs.config({map:' + JSON.stringify(jsManifest) + '});\n';
+		var seajsConfig = fs.readFileSync(seajsConfigPath, 'utf-8');
+		seajsConfig += configString;
+
+		var hash = md5(seajsConfig).slice(0, 8);
+		var ext = path.extname(seajsConfigPath);
+		//var filename = path.basename(seajsConfigPath, ext) + '-' + hash + ext;
+		var filename = path.basename(seajsConfigPath, ext) + ext;
+
+		var opts = {
+			path: filename,
+			merge: false,
+		}
+
+		getNewConfig(opts,function(err,newfile){
+			if (err) {
+				cb(err);
+				return;
+			}
+			newfile.contents = new Buffer(seajsConfig);
+			this.push(newfile);
+			cb();
+		}.bind(this))
+		
+	});
+};
+
+module.exports = plugin;
